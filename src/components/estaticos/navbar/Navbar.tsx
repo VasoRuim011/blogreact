e5f31f20aa745fea25596043608b8366383209a5
VasoRuim011 <rafaelsouza478@gmail.com>
2023-03-02 19:02:49 -0300

    02/03

---
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {AppBar, Toolbar, Typography, Box} from '@material-ui/core';
+
+
+
+function Navbar(){
+    return (
+        <>
+            <h1>Navbar</h1>
+        </>
+    )
+
+}
+
+export default Navbar;
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import './Home.css';
-import fundoHome from './https://i.imgur.com/eH6rHyh.jpg';
 
-let nome = 'Rafael Souza'
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://i.imgur.com/eH6rHyh.jpg" alt="imagem tela inicial" className="img" />
+            <h1>Home</h1>
         </>
     );
 
