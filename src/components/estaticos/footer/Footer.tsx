0de6987887ccb1f72d83401fca017ae95f43197f
VasoRuim011 <rafaelsouza478@gmail.com>
2023-03-15 18:46:49 -0300

    redux

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -1,45 +1,67 @@
 import React from 'react';
 import InstagramIcon from '@material-ui/icons/Instagram';
-import FacebookIcon from '@material-ui/icons/Facebook';
+import GitHubIcon from '@material-ui/icons/GitHub';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import {Typography, Grid } from '@material-ui/core';
 import {Box} from '@mui/material';
+import './Footer.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { useNavigate } from 'react-router-dom';
+import { addToken } from '../../../store/tokens/Actions';
 
 function Footer() {
-    return (
-        <>
-            <Grid container direction="row" justifyContent="center" alignItems="center">
+    
+        const token = useSelector<TokenState, TokenState["tokens"]>(
+            (state) => state.tokens
+        );
+        let navigate = useNavigate();
+        const dispatch = useDispatch();
+    
+        function goLogout() {
+            dispatch(addToken(''));
+            alert("Usuário deslogado");
+            navigate("/login");
+        }
+    
+        var footercomponent
+        if(token !=""){
+            footercomponent = <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3F51B5", height: "120px" }}>
+                    <Box className = "box1">
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h5" align="center" gutterBottom style={{ color: "white" }}>Siga-nos nas redes sociais </Typography>
+                            <Typography variant="h5" align="center" gutterBottom className="texto">Siga-me nas redes sociais </Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
-                            <a href="https://www.facebook.com/generationbrasil" target="_blank" rel="noopener noreferrer">
-                                <FacebookIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.instagram.com/kathllynleticia/" target="_blank" rel="noopener noreferrer">
+                                <InstagramIcon className="redes"/>
                             </a>
-                            <a href="https://www.instagram.com/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <InstagramIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://www.linkedin.com/in/kathllynleticiadesenvolvedora/" target="_blank" rel="noopener noreferrer">
+                                <LinkedInIcon className="redes"/>
                             </a>
-                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank" rel="noopener noreferrer">
-                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            <a href="https://github.com/KathllynLeticia/" target="_blank" rel="noopener noreferrer">
+                                <GitHubIcon className="redes" />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#303F9F", height: "60px" }}>
+                    <Box className=''>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className="texto">© 2023 Copyright:</Typography>
                         </Box>
                         <Box>
-                            <a target="_blank" href="https://brasil.generation.org" rel="noopener noreferrer">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">brasil.generation.org</Typography>
+                            <a rel="noopener noreferrer">
+                                <Typography variant="subtitle2" gutterBottom className = "texto" align="center">Kathllyn Santos</Typography>
                             </a>
                         </Box>
                     </Box>
                 </Grid>
             </Grid>
-        </>
-    )
+}
+return (
+    <>
+    {footercomponent}
+    </>
+)
 }
 
 export default Footer;
